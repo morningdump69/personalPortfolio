6aede3b556d5959d7f98d4e4fa8b114f70738e51
rossburrows <rossburrows96@gmail.com>
2020-02-27 17:06:04 +0000

    more home added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,26 @@
 import React from "react";
+import "../CSS/Home.css";
 
 const Home = () => {
   return (
     <div className="Heading">
       <p> Hi I'm Ross</p>
-      <p>I am currently 19 years old and i am an aspiring junior developer</p>
+      <p>
+        I am currently 19 years old and I am an aspiring junior developer, I
+        have developed my skills along side code nation, I first had an interest
+        in their course when a family member showed me a ad in the local
+        newspaper. I attended a 2019 develope course which lasted a total of
+        three weeks long where I learn the basics of web design and app
+        development, I enjoyed my time there and decided that I would like to
+        attend the master course starting in 2020 which I am currently 7 weeks
+        through.
+      </p>
+      <div className="Para2">
+        <p>
+          My past background is a Mechanical Engineering background, I completed
+          2 years{" "}
+        </p>
+      </div>
     </div>
   );
 };
