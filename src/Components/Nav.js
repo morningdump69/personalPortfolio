2e554b2e980762d32c7695cc61f7e0a1a6ddabb7
rossburrows <rossburrows96@gmail.com>
2020-02-28 11:59:42 +0000

    homepage fix

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,9 +4,15 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <div className="navbar">
-      <Link to="/"> Home </Link>
-      <Link to="/Projects"> Projects </Link>
-      <Link to="/Contact"> Contact</Link>
+      <div className="home">
+        <Link to="/"> Home </Link>
+      </div>
+      <div className="projects">
+        <Link to="/Projects"> Projects </Link>
+      </div>
+      <div className="contact">
+        <Link to="/Contact"> Contact</Link>
+      </div>
     </div>
   );
 };
