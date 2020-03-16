0336669a8912abfc9a37f5da995fb71d624007ca
rossburrows <rossburrows96@gmail.com>
2020-03-16 17:16:32 +0000

    Updates

---
+++ precache-manifest.48d171d4e5d6d350d014d51ee6ea81a9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1723bbb89c9f512b92f0765573f43321",
+    "url": "/personalPortfolio/index.html"
+  },
+  {
+    "revision": "349c24f5efe41ff71d2a",
+    "url": "/personalPortfolio/static/css/main.b535f5ee.chunk.css"
+  },
+  {
+    "revision": "27822f96a7d96b711d62",
+    "url": "/personalPortfolio/static/js/2.0512ea3f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/personalPortfolio/static/js/2.0512ea3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349c24f5efe41ff71d2a",
+    "url": "/personalPortfolio/static/js/main.23794b4f.chunk.js"
+  },
+  {
+    "revision": "37fbf745af8694cf8797",
+    "url": "/personalPortfolio/static/js/runtime-main.b86aea7c.js"
+  },
+  {
+    "revision": "23b7f180df2ac306fa8f95152f053d0b",
+    "url": "/personalPortfolio/static/media/Drumkit.23b7f180.png"
+  },
+  {
+    "revision": "634923a0abef61863d6ef21852723d90",
+    "url": "/personalPortfolio/static/media/Fail.634923a0.jpg"
+  },
+  {
+    "revision": "cd79078a9b757f24e7adc2dca829dd25",
+    "url": "/personalPortfolio/static/media/Keycode.cd79078a.png"
+  },
+  {
+    "revision": "99f4362fa1f4efa65d186c6c35581f65",
+    "url": "/personalPortfolio/static/media/Myself.99f4362f.jpg"
+  },
+  {
+    "revision": "1e9011214c500912261d2717aa2890f9",
+    "url": "/personalPortfolio/static/media/Todolist.1e901121.png"
+  },
+  {
+    "revision": "a6907fab06ff09228ac618cfed59c6db",
+    "url": "/personalPortfolio/static/media/calculator.a6907fab.png"
+  },
+  {
+    "revision": "655bbe71c094f0bcf6e1bd623a32f784",
+    "url": "/personalPortfolio/static/media/cardgame.655bbe71.png"
+  },
+  {
+    "revision": "3d3bba5dddbf96138561b889a145589e",
+    "url": "/personalPortfolio/static/media/coffee.3d3bba5d.png"
+  },
+  {
+    "revision": "55f093d93455d6fd427b8fd772c7d8c8",
+    "url": "/personalPortfolio/static/media/diegame.55f093d9.png"
+  },
+  {
+    "revision": "624707f58190751debd06b1fd4093252",
+    "url": "/personalPortfolio/static/media/firstwebsite.624707f5.png"
+  },
+  {
+    "revision": "f23e1f9b145db3774c0b73efb8e5239a",
+    "url": "/personalPortfolio/static/media/fruitmachine.f23e1f9b.png"
+  },
+  {
+    "revision": "93df7dac7a577e1fd08017490293446d",
+    "url": "/personalPortfolio/static/media/gaming.93df7dac.jpg"
+  }
+]);
